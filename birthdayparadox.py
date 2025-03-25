cf78b27902de2b06fc585cd7df71afc4209ccdf2
mjones2453 <mjones942676@gmail.com>
2025-03-25 09:54:29 -0400

    update birthdayparadox.py

--- birthdayparadox.py
+++ birthdayparadox.py
@@ -46,6 +46,61 @@ simulations) to explore this concept.
 MONTHS = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
           'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
 
+while True:
+    print('How many birthdays shall I generate? (Max 100)')
+    response = input('> ')
+    if response.isdecimal() and (0 < int(response) <= 100):
+        num_b_days = int(response)
+        break # User has entered an invalid amount.
+print()
 
+# Generate and display the birthdays:
+print('Here are', num_b_days, 'birthdays:')
+birthdays = get_birthdays(num_b_days)
+for i, birthday in enumerate(birthdays):
+    if i != 0:
+        # Display a comma for each birthday after the first birthday.
+        print(',', end='')
+    month_name = MONTHS[birthday.month - 1]
+    date_text = f'{month_name} {birthday.day}'
+    print(date_text, end='')
 
+print()
+print()
 
+# Determine if there are two birthdays that match.
+match = get_match(birthdays)
+
+# Display the results:
+print("In this simulation, ", end='')
+if match != None:
+    month_name = MONTHS[match.month - 1]
+    date_text = f'{month_name} {match.day}'
+    print('multiple people have a birthday on', date_text)
+else:
+    print('there are no matching birthdays.')
+print()
+
+# Run through 100,000 simulations:
+print('Generating', num_b_days, 'random birthdays 100,000 times...')
+input('Press Enter to begin...')
+
+print('Let\'s run another 100,000 simulations.')
+sim_match = 0 # How many simulations had matching birthdays in them.
+for i in range(100000):
+    # Report on the progress every 10,000 simulations:
+    if i % 10000 == 0:
+        print(i, 'simulations run...')
+    birthdays = get_birthdays(num_b_days)
+    if get_match(birthdays) != None:
+        sim_match += 1
+print('100,000 simulations have been run.')
+
+# Display simulation results:
+probability = round(sim_match / 100000 * 100, 2)
+print()
+print('Out of 100,000 simulations of', num_b_days, 'people, there was a' )
+print('matching birthday in that group', sim_match, 'times. This means')
+print('that', num_b_days, 'people have a', probability, '% chance of')
+print('having a matching birthday in their group.')
+print('That\'s probably more than you would think!')
